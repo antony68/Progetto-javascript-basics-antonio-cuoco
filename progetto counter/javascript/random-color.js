c94c8f1ded39073ba771abd9fffbf8dc1581bb0a
antony68 <30431200+antony68@users.noreply.github.com>
2022-11-05 11:43:45 +0100

    Add files via upload

---
+++ progetto counter/javascript/random-color.js
@@ -0,0 +1,9 @@
+const setBg = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    int.style.color = "#" + randomColor;
+    colorSpan.innerHTML = "#" + randomColor;
+    colorSpan.style.color = "#" + randomColor; 
+  }
+  
+  reset.addEventListener("click",setBg);
+  setBg();
